b769dc45d4a4f1e300780cfc52a13929691329d3
vas g <vas.g@me.com>
2023-04-25 06:47:07 +0400

    add location keyboard

--- resources/keyboards.py
+++ resources/keyboards.py
@@ -5,3 +5,7 @@ exchange = InlineKeyboardButton(text='Конвертер валют', callback_d
 random_pic = InlineKeyboardButton(text='Картинка', callback_data='random_pic')
 
 menu = InlineKeyboardMarkup(row_width=1).add(weather, exchange, random_pic)
+
+
+provide_location_button = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+provide_location_button.insert(KeyboardButton(text='Отправить геопозицию', request_location=True))
