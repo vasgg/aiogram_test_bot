d3547b0d73f7bc36ade57e7a4ed22a888a5dba5b
vas g <vas.g@me.com>
2023-04-25 06:48:30 +0400

    add weather from location

--- handlers/weather.py
+++ handlers/weather.py
@@ -3,14 +3,14 @@ from aiogram.dispatcher import FSMContext
 
 from config import dp
 from resources.weather import Weather, get_weather_from_message
-from resources.keyboard import menu
+from resources.keyboards import menu, provide_location_button
 from resources.replies import answer
 from resources.states import States
 
 
 @dp.callback_query_handler(text='weather')
 async def weather_section(call: types.CallbackQuery) -> None:
-    await dp.bot.send_message(chat_id=call.message.chat.id, text=answer['input_city_reply'])
+    await dp.bot.send_message(chat_id=call.message.chat.id, text=answer['input_city_reply'],reply_markup=provide_location_button)
     await States.weather_city_input.set()
     await call.answer()
 
@@ -19,8 +19,8 @@ async def weather_section(call: types.CallbackQuery) -> None:
 async def weather_in_city(message: types.Message, state: FSMContext) -> None:
     try:
         weather: Weather = await get_weather_from_message(message.text)
-        response = answer['weather_reply'].format(weather.city, weather.info, weather.temp)
-        await dp.bot.send_message(chat_id=message.chat.id, text=response, reply_markup=menu)
+        reply = answer['weather_reply'].format(weather.city, weather.info, weather.temp)
+        await dp.bot.send_message(chat_id=message.chat.id, text=reply, reply_markup=menu)
         await state.reset_state()
     except Exception:
         await dp.bot.send_message(chat_id=message.from_user.id, text=answer['error_fetch_weather'])
--- resources/weather.py
+++ resources/weather.py
@@ -2,7 +2,7 @@ import json
 
 import aiohttp.client
 
-from config import weather_apikey, WEATHER_URL
+from config import weather_apikey, WEATHER_GEO_URL, WEATHER_URL
 
 
 class Weather:
@@ -16,10 +16,14 @@ class Weather:
 async def get_weather_from_message(city_name: str) -> Weather:
     return await make_weather_service_query(get_city_weather_url(city_name))
 
+async def get_weather_from_location(latitude: float, longitude: float) -> Weather:
+    return await make_weather_service_query(get_city_weather_url_from_location(latitude, longitude))
 
-def get_city_weather_url(city_name: str):
+def get_city_weather_url(city_name: str) -> str:
     return WEATHER_URL.format(city_name, weather_apikey)
 
+def get_city_weather_url_from_location(latitude: float, longitude: float) -> str:
+    return WEATHER_GEO_URL.format(latitude, longitude, weather_apikey)
 
 async def make_weather_service_query(url: str) -> json:
     async with aiohttp.ClientSession() as session:
